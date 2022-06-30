2d80f17a78439ce1810657f66224e7ce2b4fc8b6
doubtcrack <1.edujock@gmail.com>
2022-06-30 13:14:54 +0530

    folder structure setup

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-const app = require('express')()
-// const ejs = require('ejs')
-const path = require('path')
-const expressLayout = require('express-ejs-layouts')
-const PORT = process.env.PORT || 3300
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
+const PORT = process.env.PORT || 3300;
 
-app.get("/", (req, res)=>{
-    res.render('home')
-})
+//Assets
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 // set Template engine
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
-app.listen(PORT , () => {
-            console.log(`Listening on port ${PORT}`)
-        })
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
